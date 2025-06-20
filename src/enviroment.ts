4c87ec92d8a6a440ae064fde13a85691cca0417b
Sebastián <seb4ndre@gmail.com>
2025-06-19 22:48:18 -0500

    Archivo de enviroment y de Rutas

---
+++ src/enviroment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  //URL_Api: "http://localhost:44353/",
+  URL_Api: "https://smart.ssnf.gob.pa/web_smart_api/",
+  URL_RUBF_INTERNO: "",
+  production: false,
+  enviaCorreo: true,
+  version: "1.1.1"
+};
