c3d64cda9b17ec026dba124749c462bc86c78c9a
Seb4ndre <seb4ndre@gmail.com>
2025-07-05 13:34:36 -0500

    Arreglo de enlace

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  //URL_Api: "http://seb4ndre-001-site1.qtempurl.com/API",
-  URL_Api: "http://localhost:50765/",
+  URL_Api: "http://seb4ndre-001-site1.qtempurl.com/API",
+  //URL_Api: "http://localhost:50765/",
   production: false,
   enviaCorreo: true,
   version: "1.1.1"
