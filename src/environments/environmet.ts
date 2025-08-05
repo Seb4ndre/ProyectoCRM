8b90b97669bd7802cdf2edd2d74321b86dbd03dc
Sebastián <seb4ndre@gmail.com>
2025-08-04 22:48:22 -0500

    Subida de APAPROPH

--- src/environments/environmet.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  //URL_Api: "http://seb4ndre-001-site1.qtempurl.com/API",
-  URL_Api: "http://localhost:50765",
-  production: false,
-  enviaCorreo: true,
-  version: "1.1.1"
-};
